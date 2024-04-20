8ec74c19ffc40b518fa49023ccae786647cff994
Ekarat <ekarattanagart@Ekarats-MacBook-Air.local>
2024-04-20 13:20:40 +0700

    add view to update courses module using module formset

--- courses/urls.py
+++ courses/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<pk>/delete', views.CourseDeleteView.as_view(), name='course_delete'),
     path('login', views.login_page, name='login'),
     path('logout', views.logout_page, name='logout'),
+    path('<pk>/module', views.CourseModuleUpdateView.as_view(), name='course_module_update'),
 ]
